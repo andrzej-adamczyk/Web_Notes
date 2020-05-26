1a58e4cf442a1a06571f745476d56e0ffadd98dc
Andrzej Adamczyk <andrzej.adamczyk@protonmail.com>
2020-05-26 10:34:53 +0200

    added new files

--- scripts.js
+++ scripts.js
@@ -7,5 +7,5 @@
 // const nav1 = document.querySelector('navigation--js');
 // console.log(nav1);
 
-const nav_ = document.getElementsByClassName('.navigation')
+const nav_ = document.querySelector('body')
 console.log(nav_)
