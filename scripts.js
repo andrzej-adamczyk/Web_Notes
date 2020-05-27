2ed06d11e5174de5b64c9abbf5d417e2c3f1265c
Andrzej Adamczyk <adam.griffin.usa@gmail.com>
2020-05-27 20:41:58 +0200

    merged classes in linki.html

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
-// var imie = "Andrzej";
+var imie = "Andrzej";
 
-// var nazwisko = "Adamczyk";
+var nazwisko = "Adamczyk";
 
-// alert(`Witam, z tej strony ${imie} ${nazwisko}`);
+alert(`Witam, z tej strony ${imie} ${nazwisko}`);
 
-// const nav1 = document.querySelector('navigation--js');
-// console.log(nav1);
+const nav_ = document.querySelector("h1");
+console.log(nav_);
 
-const nav_ = document.querySelector('h1')
-console.log(nav_)
+// nav_.innerHTML = "<strong>dupa</strong>";
+
+// console.log(nav_);
+
+const empty = document.querySelector(".empty__element");
+empty.innerHTML = "Tekst wygenerowany przez Java Script";
