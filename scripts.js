63abb0e5290346d03527bbb98ffece5314f0d17e
Andrzej Adamczyk <adam.griffin.usa@gmail.com>
2020-05-29 13:26:22 +0200

    funkcje JS

--- scripts.js
+++ scripts.js
@@ -2,7 +2,9 @@ var imie = "Andrzej";
 
 var nazwisko = "Adamczyk";
 
-alert(`Witam, z tej strony ${imie} ${nazwisko}`);
+const age = 30;
+
+// alert(`Witam, z tej strony ${imie} ${nazwisko}`);
 
 const nav_ = document.querySelector("h1");
 console.log(nav_);
@@ -12,4 +14,45 @@ console.log(nav_);
 // console.log(nav_);
 
 const empty = document.querySelector(".empty__element");
-empty.innerHTML = "Tekst wygenerowany przez Java Script";
+empty.innerHTML = "***Tekst wygenerowany przez Java Script***";
+
+function calculate(myNumber) {
+  var outcome = myNumber * 7;
+  console.log(`Dostałem ${myNumber}`);
+  console.log(outcome);
+  return `Dostałem ${myNumber * myNumber}`;
+}
+
+function greet(imie, nazwisko) {
+  console.log(`Cześć ${imie} ${nazwisko}, masz ${age} lat`);
+}
+
+greet("Ada", "Adam");
+
+calculate(7);
+
+const myResult = calculate(age);
+
+console.log(myResult);
+
+myElement = "empty__fillContent";
+
+myContent = "***Element uzupełniony przez funkcje JS fillContent()***";
+
+function fillContent(element, content) {
+  const emptyforFunction = document.querySelector(`.${myElement}`);
+  emptyforFunction.innerHTML = myContent;
+}
+
+fillContent(myElement, myContent);
+
+myElement = "empty__updateContent";
+
+myContent = "***Element nadpisany przez funkcje JS updateContent()***";
+
+const update_content = (element, content) => {
+  const emptyforFunction = document.querySelector(`.${myElement}`);
+  emptyforFunction.innerHTML = myContent;
+};
+
+update_content(myElement, myContent);
